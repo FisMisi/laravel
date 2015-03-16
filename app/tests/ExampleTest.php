a902890bdf6324506acf029fce0c5b4702709a51
Markó Mihály <Mark Mihly@10.200.0.38>
2015-03-16 16:25:55 +0100

    hétfő délután

--- app/tests/ExampleTest.php
+++ app/tests/ExampleTest.php
@@ -1,17 +1,36 @@
 <?php
 
-class ExampleTest extends TestCase {
+class ExampleTest extends TestCase 
+{
 
-	/**
-	 * A basic functional test example.
-	 *
-	 * @return void
-	 */
-	public function testBasicExample()
-	{
-		$crawler = $this->client->request('GET', '/');
-
-		$this->assertTrue($this->client->getResponse()->isOk());
-	}
-
+  public static function getCategories($type, $modelId)
+    {   #DB::enableQueryLog();
+        $query = self::leftJoin('model_model_category', function($join) use($modelId)
+            {
+            $join->on('model_model_category.category_id', '=', 'model_categories.id')
+                ->where('model_model_category.model_id', '=', $modelId);
+            })
+                ->where('model_categories.type_id', '=', $type)
+                ->where('model_categories.active', '=', 1);
+        $ret = $query->get(array('model_categories.id','model_categories.title','model_model_category.model_id'))->toArray();     
+        /*$queries = DB::getQueryLog();
+        $last_query = end($queries);
+        var_dump($last_query);*/
+        return $ret;
+    }
+    
+//    <h2>Your Categories</h2>    
+//  @foreach($helperDataJson['categoryTypes'] as $type)
+//    <div>
+//        <b>{{ $type['title'] }}</b>
+//        @foreach(ModelCategory::getCategories($type['id'],$helperDataJson['userModel']->id) as $category)
+//            @if($type['multi'] == 1)
+//               {{ Form::checkbox($type['name'].'[]',$category['id'],!is_null($category['model_id'])) }} {{ $category['title'] }}  
+//            @else
+//               {{ Form::radio($type['name'],$category['id'],!is_null($category['model_id'])) }} {{ $category['title'] }}
+//            @endif 
+//        @endforeach
+//    </div>
+//    @endforeach 
+//  </div>
 }
