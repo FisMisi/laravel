720b2f5b3b4fe9a30f1435bb5f28e3d5a9d10918
Markó Mihály <Mark Mihly@10.200.0.38>
2015-03-26 16:16:56 +0100

    admin oldalon l+ languages + video categories lekezelvessssssssssssssssasadasdas

--- app/tests/ExampleTest.php
+++ app/tests/ExampleTest.php
@@ -74,3 +74,62 @@ class ExampleTest extends TestCase
                 }
 
 }
+
+public static function downloadvideos() 
+    {
+         $query = self::join('storaged_video_types', 'storaged_video_types.id', '=', 'storaged_videos.type_id')
+                     ->leftJoin('models', 'storaged_videos.model_id', '=', 'models.id');
+        
+         $getArray = array("videos.video_id",
+                          "base_video_id",
+                          "length",
+                          "active",
+                          "active2",
+                          "created_at",
+                          "rating",
+                          "rating_l",
+                          "sum_rating",
+                          "sum_rating_l",
+                          "rating_number",
+                          "rating_number_l",
+                          "see_count",
+                          "see_count_l");
+
+        $count = $query->count();
+        $page = ceil($count/200);
+        $basePath = storage_path();
+        if (!file_exists($basePath.'/videoexport')) {
+                mkdir($basePath.'/videoexport', 0770, true);
+        }
+        $path = $basePath.'/videoexport/';
+        $file = "videos".date("Y_m_d_h_i_s").".csv";
+        $del = ',';
+        $newRow = "\n";	
+        $exportData = '';
+        $exportData.= 'video id'.$del.'base id'.$del.'video length'.$del.'active by tags'.$del.'active'.$del.'created'.$del.'rating all'.$del.'local rating'.$del.'sum rating all'.$del.'rating number all'.$del.'sum rating local'.$del.'rating number local'.$del.'see video all'.$del.'see video local'.$newRow;
+        for($p = 0;$p < $page; $p++) {
+                file_put_contents($path."rows.txt", $p);
+                $idDatas = $query->take(200)->skip($p*200)->get($getArray)->toArray();
+                foreach($idDatas as $row) {
+                        $exportData.= $row['video_id'].$del
+                                     .$row['base_video_id'].$del
+                                     .$row['length'].$del
+                                     .$row['active'].$del
+                                     .$row['active2'].$del
+                                     .$row['created_at'].$del
+                                     .$row['rating'].$del
+                                     .$row['rating_l'].$del
+                                     .$row['sum_rating'].$del
+                                     .$row['rating_number'].$del
+                                     .$row['sum_rating_l'].$del
+                                     .$row['rating_number_l'].$del
+                                     .$row['see_count'].$del
+                                     .$row['see_count_l']
+                                     .$newRow;
+                }
+                file_put_contents($path.$file, $exportData, FILE_APPEND | LOCK_EX);
+                $exportData = '';
+        }
+
+        return Response::download($path.$file, $file);
+    }
